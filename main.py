32db6a6c5e9b68c1af39d26d99c005a750fd46b8
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-08-26 22:13:23 +0800

    add light_field.py, add bilinear;

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import cv2
 import time
 
 from spectrum import *
+from light_field import *
 
 if __name__ == '__main__':
     print('start.')
     # lena = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
 
-    s = Spectrum(name = 'cos')
-    # s.matrix = lena
-    # s.set_cos()
-    s.set_rect(1,1)
-    s.fourier()
-    s.show()
+    s = Spectrum(name='cos')
+    r = Ray(0, 0, s) # TODO: point, direction
+    # r.matrix = lena
+    # r.set_cos()
+    r.set_rect(1,1)
+    r.travel(0.2)
+    r.show()
--- spectrum.py
+++ spectrum.py
@@ -3,10 +3,14 @@ from pylab import *
 import matplotlib.pyplot as plt
 
 PI = 3.1415926
+UNDEFINED = 0
 
 class Spectrum():
+    '''
+    matrix[x, theta]
+    '''
 
-    def __init__(self, dims=2, radius=10, sampling_rate=1000, name='unnamed spectrum'):
+    def __init__(self, dims=2, radius=10, sampling_rate=100, name='unnamed spectrum'):
         assert (dims==2), "only 2-d examples is supported yet."
         self.time = 0
         self.name = name
@@ -14,13 +18,13 @@ class Spectrum():
         self.radius = radius
         self.sampling_rate = sampling_rate
         self.width = 2 * self.radius / self.sampling_rate
-        self.reset_matrix()
-        self.reset_Fmatrix()
+        self.init_matrix()
+        self.init_Fmatrix()
 
-    def reset_matrix(self):
+    def init_matrix(self):
         self.matrix = np.zeros(tuple(self.sampling_rate for i in range(self.dims)))
 
-    def reset_Fmatrix(self):
+    def init_Fmatrix(self):
         self.Fmatrix = np.zeros(tuple(self.sampling_rate for i in range(self.dims)))
 
     def set_cos(self):
@@ -29,19 +33,41 @@ class Spectrum():
         normalized_cos_list = normalized_cos_list.astype(np.uint8)
         normalized_cos_list = np.expand_dims(normalized_cos_list, 0)
         cos_matrix = np.repeat(normalized_cos_list, 1000, axis=0)
+        del self.matrix
+        del self.Fmatrix
         self.matrix = cos_matrix
+        self.fourier()
 
     def set_rect(self, width, height, value=255):
         w_sampling_radius = int(width/self.radius*self.sampling_rate/2)
         h_sampling_radius = int(height/self.radius*self.sampling_rate/2)
         w_sampling_start = int(self.sampling_rate/2 - w_sampling_radius)
         h_sampling_start = int(self.sampling_rate/2 - h_sampling_radius)
-        self.reset_matrix()
+        del self.matrix
+        del self.Fmatrix
+        self.init_matrix()
         self.matrix[h_sampling_start:h_sampling_start+h_sampling_radius*2,\
             w_sampling_start:w_sampling_start+w_sampling_radius*2] = value
+        self.fourier()
     
     def fourier(self):
         self.Fmatrix = np.fft.fftshift(np.fft.fft2(self.matrix))
+
+    def bilinear(self, m, i, j):
+        i = i+self.sampling_rate/2
+        j = j+self.sampling_rate/2
+        i_floor = int(np.floor(i))
+        i_ceil = int(np.ceil(i))
+        j_floor = int(np.floor(j))
+        j_ceil = int(np.ceil(j))
+        if not all([x < self.sampling_rate and x >= 0 
+            for x in (i_floor, i_ceil, j_floor, j_ceil)]): # TODO: make more accurate
+            return UNDEFINED
+        res_floor = np.interp(i, [i_floor, i_ceil], [m[i_floor, j_floor], m[i_ceil, j_floor]])
+        res_ceil = np.interp(i, [i_floor, i_ceil], [m[i_floor, j_ceil], m[i_ceil, j_ceil]])
+        res = np.interp(j, [j_floor, j_ceil], [res_floor, res_ceil])
+
+        return res
 
     def show(self):
         fig = plt.figure()
