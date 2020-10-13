1addfc9616ba7e7f120ea30e5b8f9df8e3214d17
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-10-13 21:47:53 +0800

    add cov;

---
+++ covariance.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from spectrum4d import *
+
+class Covariance(Spectrum4d):
+
+    def __init__(self, s):
+        super(Covariance, self).__init__(dims = s.dims, radius = s.radius, 
+            sampling_rate=s.sampling_rate, name=s.name, 
+            fake_bilinear = s.fake_bilinear)
+        self.m = np.identity(4)
