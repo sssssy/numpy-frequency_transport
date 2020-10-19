5d97c257a301107a9c4ed72dead5683425212a5e
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-10-19 21:12:12 +0800

    add bsdf_cov;

--- covariance.py
+++ covariance.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import pinv
 
 from spectrum4d import *
 
@@ -6,7 +7,7 @@ class Covariance(Spectrum4d):
 
     def __init__(self, s):
 
-        super(Covariance, self).__init__(dims = s.dims, radius = s.radius, 
+        super(Covariance, self).__init__(dims = s.dims, 
             sampling_rate=s.sampling_rate, name=s.name, 
             fake_bilinear = s.fake_bilinear)
         self.cov = None
@@ -32,10 +33,11 @@ class Covariance(Spectrum4d):
         '''
         calculate covariance matrix of m
         '''
-        cov = np.identity(self.dims)
+        d = len(m.shape)
+        cov = np.identity(d)
         
-        for dim_0 in range(self.dims):
-            for dim_1 in range(self.dims):
+        for dim_0 in range(d):
+            for dim_1 in range(d):
                 integral = 0
                 # calculate integral
                 for i in range(self.sampling_rate):
@@ -45,7 +47,7 @@ class Covariance(Spectrum4d):
                                 e = np.array([i, j, k, l]) / self.sampling_rate - 0.5
                                 integral += e[dim_0] * e[dim_1] * np.abs(m[i, j, k, l])
                 cov[dim_0, dim_1] = integral
-                
+        
         return cov
 
     def apply_transform(self):
@@ -93,7 +95,7 @@ class Covariance(Spectrum4d):
             for j in range(self.sampling_rate):
                 for k in range(self.sampling_rate):
                     for l in range(self.sampling_rate):
-                        cos_light_field[i, j, k, l] = max(0, np.cos(k - self.sampling_rate // 2 + theta_in))
+                        cos_light_field[i, j, k, l] = max(0, np.cos(k - self.radius + theta_in))
         
         # get spectrum, get covariance matrix
         cos_spectrum = np.fft.fftshift(np.fft.fftn(cos_light_field, axes=(0,1,2,3)))
@@ -101,6 +103,21 @@ class Covariance(Spectrum4d):
 
         # add it to self.cov
         self.cov += cos_spectrum_cov
+
+    def bsdf_cov(self):
+
+        # get bsdf spectrum cov in 4d
+        bsdf_dist = self.bsdf[self.radius]
+        var = bsdf_dist.var()
+        bsdf_dist_cov = np.zeros(self.cov.shape)
+        bsdf_dist_cov[2, 2] = var
+        print('bsdf_dist_cov: \n{}'.format(bsdf_dist_cov))
+
+        # We approximate it using the formula for zero-centered
+        # Gaussians multiplication. When we multiply two zero-centered Gaussians, 
+        # the resulting distribution is a Gaussian whose inverse covariance matrix is 
+        # the sum of input inverse covariance matrices.
+        self.cov = pinv(pinv(self.cov) + pinv(bsdf_dist_cov))
         
     def print_cov(self):
 
